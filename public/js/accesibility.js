c052938d2095deb4f4bf64ea0dacbc32c02ee42e
Francisco Miguez <franmiguez64@gmail.com>
2025-12-18 08:47:04 -0500

    Mejoras de accesibilidad - INDEX

--- public/js/accesibility.js
+++ public/js/accesibility.js
@@ -1,3 +1,19 @@
-$(".accesibility-menu").click(() => {
-    alert("hio");
+$(document).ready(() => {
+    $(".ic-hi-con").click(() => {
+        $("body").toggleClass("high-contrast");
+    });
+
+    $(".ic-si-inc").click(() => {
+        const root = document.documentElement;
+        let size = parseFloat(getComputedStyle(root).getPropertyValue("--text-size"));
+        if (size > 1.5) return;
+        root.style.setProperty("--text-size", (size + 0.25) + "em");
+    });
+
+    $(".ic-si-dec").click(() => {
+        const root = document.documentElement;
+        let size = parseFloat(getComputedStyle(root).getPropertyValue("--text-size"));
+        if (size < 1.5) return;
+        root.style.setProperty("--text-size", (size - 0.25) + "em");
+    });
 });
